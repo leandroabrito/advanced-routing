430fe523f630d9d6fd2c10832d0031969c1256df
leandro brito <leandroabrito89@gmail.com>
2020-07-21 18:17:37 -0300

    seed restaurants with chef_name

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,7 +9,8 @@ puts 'creating restaurants'
   restaurant = Restaurant.create(
     name: Faker::Restaurant.name,
     address: Faker::Address.street_address,
-    rating: rand(1..5)
+    rating: rand(1..5),
+    chef_name: [ 'Gordon Ramsey', 'Gido', 'Alain Ducasse', 'Jamie Oliver' ].sample
   )
   puts "restaurant #{restaurant.id} is created"
 end
