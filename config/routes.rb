aee1801d5b8cd2406b60dabd67b07587c0746c4d
leandro brito <leandroabrito89@gmail.com>
2020-07-21 18:28:32 -0300

    add restaurant chef feature -> routes + controller + view

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,14 @@
 Rails.application.routes.draw do
   resources :restaurants do
+
     collection do
       get :top # /restaurants/top
     end
+
+    member do
+      get :chef
+    end
+
   end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
