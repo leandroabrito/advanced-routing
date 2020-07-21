4582052859b81c8fb8389a27f2da7caf9730b41b
leandro brito <leandroabrito89@gmail.com>
2020-07-21 18:54:27 -0300

    add review feature -> route + controller + view

--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,7 @@ Rails.application.routes.draw do
       get :chef
     end
 
+    resources :reviews, only: [ :new, :create ]
   end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
